4198e804e64786e400904ec688e4b0b46f1ec1c6
Angelo Canunayon <angelocanunayon@gmail.com>
2025-09-26 08:29:52 -0500

    Initial portfolio commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Portfolio from './Portfolio';
+import portfolio from './portfolio';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
